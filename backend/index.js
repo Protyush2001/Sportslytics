51bff4c385cd45a3766113458d69f966b2040e5f
Protyush2001 <deyprotyush@gmail.com>
2025-12-13 21:53:01 +0530

    cors change

--- backend/index.js
+++ backend/index.js
@@ -60,10 +60,7 @@ const upload = multer({
 const io = new Server(server, {
   cors: {
     origin: [
-      "http://localhost:5173",
-      "http://localhost:3000",
-      "http://127.0.0.1:5173",
-      "http://localhost:5174"
+      '*'
     ],
     methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
   },
